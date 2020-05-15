5ce52af002fa30019239bd72e922eacc0934176a
NC-Coyote <006765987@coyote.csusb.edu>
2020-05-15 16:06:30 -0700

    Scene Transition Update
    -Added fade-to-black scene exit
    -Added fade-in-from-black scene entrance
    -Player will also change scenes when entering the exit zone trigger collider
        Scenes change from NateScene to NateScene2 and back
    
    Only the right side of NateScene has a single exit point and the left side of NateScene2 has a single exit point to transition between the 2 maps for proof of concept.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+    public string areaToLoad;
+    public string areaTransitionName;
+
+    public float transitionTime = 0.5f;
+
+    // Update is called once per frame
+    void Start()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(LoadLevel(areaToLoad));
+        }
+    }
+
+    IEnumerator LoadLevel(string levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+        PlayerController.instance.areaTransitionName = areaTransitionName;
+    }
+}
