9a87fd65a3effd40b5491f7db41bfe455b53872e
NC-Coyote <006765987@coyote.csusb.edu>
2020-06-05 15:34:47 -0700

    Pause Menu Implemented
    -Added a pause menu
    -Added some code to the menuController script

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,8 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Setup");
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
